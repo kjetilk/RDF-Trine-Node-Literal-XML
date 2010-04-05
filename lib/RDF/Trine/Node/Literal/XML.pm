9a8ec7c0c15561b9e69e1b5cfa38c8eab57b542f
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2010-04-05 19:40:19 +0000

    Run perltidy on the file
    git-svn-id: http://svn.kjernsmo.net/RDF-Trine-Node-Literal-XML/trunk@463 c0e5d59b-64f6-0310-af01-800fc5e6926e

--- lib/RDF/Trine/Node/Literal/XML.pm
+++ lib/RDF/Trine/Node/Literal/XML.pm
@@ -20,9 +20,10 @@ use XML::LibXML qw(:ns);
 
 ######################################################################
 
-our ($VERSION, %XML_FRAGMENTS);
+our ( $VERSION, %XML_FRAGMENTS );
+
 BEGIN {
-	$VERSION	= '0.15';
+  $VERSION = '0.15';
 }
 
 ######################################################################
@@ -61,86 +62,91 @@ of one of the above types, this method throws a RDF::Trine::Error exception.
 =cut
 
 sub new {
-	my $class	= shift;
-	my $input	= shift;
-	my $lang	= shift;
+  my $class = shift;
+  my $input = shift;
+  my $lang  = shift;
 
-	my $typeok = _check_type($input); # First check if we have a valid node
-	if ($typeok) { # Then use it
-	  my $literal;
-	  if ($input->isa('XML::LibXML::NodeList')) {
-	    foreach my $context ($input->get_nodelist) {
-	      if ($lang) {
-		$context->setAttributeNS(XML_XML_NS, 'lang', $lang);
-	      }
-	      if ($context->ownerDocument) {
-		$literal .= $context->toStringEC14N;
-	      } else {
-		$literal .= $context->toString;
-	      }
-	    }
-	  } else {
-	    if ($lang) {
-	      if ($input->isa('XML::LibXML::Element')) {
-		$input->setAttributeNS(XML_XML_NS, 'lang', $lang);
-	      }
-	      elsif ($input->isa('XML::LibXML::Document')) {
-		my $root = $input->documentElement();
-		$root->setAttributeNS(XML_XML_NS, 'lang', $lang);
-	      }
-	      elsif ($input->isa('XML::LibXML::DocumentFragment')) {
-		foreach my $context ($input->childNodes) {
-		  $context->setAttributeNS(XML_XML_NS, 'lang', $lang);
-		}
-	      } else {
-		carp ref($input) . " doesn't support xml:lang attributes";
-	      }
-	    }
-	    if ($input->ownerDocument) {
-	      $literal = $input->toStringEC14N;
-	    } else {
-	      $literal = $input->toString;
-	    }
-	  }
-	  my $self	= $class->SUPER::new( $literal, undef, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral' );
-	  $XML_FRAGMENTS{ refaddr( $self ) }	= $input;
-	  return $self;
-	}
+  my $typeok = _check_type($input);    # First check if we have a valid node
+  if ($typeok) {                       # Then use it
+    my $literal;
+    if ( $input->isa('XML::LibXML::NodeList') ) {
+      foreach my $context ( $input->get_nodelist ) {
+        if ($lang) {
+          $context->setAttributeNS( XML_XML_NS, 'lang', $lang );
+        }
+        if ( $context->ownerDocument ) {
+          $literal .= $context->toStringEC14N;
+        }
+        else {
+          $literal .= $context->toString;
+        }
+      }
+    }
+    else {
+      if ($lang) {
+        if ( $input->isa('XML::LibXML::Element') ) {
+          $input->setAttributeNS( XML_XML_NS, 'lang', $lang );
+        }
+        elsif ( $input->isa('XML::LibXML::Document') ) {
+          my $root = $input->documentElement();
+          $root->setAttributeNS( XML_XML_NS, 'lang', $lang );
+        }
+        elsif ( $input->isa('XML::LibXML::DocumentFragment') ) {
+          foreach my $context ( $input->childNodes ) {
+            $context->setAttributeNS( XML_XML_NS, 'lang', $lang );
+          }
+        }
+        else {
+          carp ref($input) . " doesn't support xml:lang attributes";
+        }
+      }
+      if ( $input->ownerDocument ) {
+        $literal = $input->toStringEC14N;
+      }
+      else {
+        $literal = $input->toString;
+      }
+    }
+    my $self = $class->SUPER::new( $literal, undef,
+      'http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral' );
+    $XML_FRAGMENTS{ refaddr($self) } = $input;
+    return $self;
+  }
 
-	if (ref($input) && (! $typeok)) { # Then it is neither a string nor a good type
-	  throw RDF::Trine::Error -text => ref($input) . " is not a valid type.";
-	}
+  if ( ref($input) && ( !$typeok ) )
+  {    # Then it is neither a string nor a good type
+    throw RDF::Trine::Error -text => ref($input) . " is not a valid type.";
+  }
 
-	# If we have an empty string, just create an empty text node
-	if (length($input) == 0) {
-	  my $self = $class->SUPER::_new( $input, undef, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral' );
+  # If we have an empty string, just create an empty text node
+  if ( length($input) == 0 ) {
+    my $self = $class->SUPER::_new( $input, undef,
+      'http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral' );
 
-	  $XML_FRAGMENTS{ refaddr( $self ) }	= XML::LibXML::Text->new($input);
-	  return $self;
-	}
+    $XML_FRAGMENTS{ refaddr($self) } = XML::LibXML::Text->new($input);
+    return $self;
+  }
 
+  # Last chance is that it is a string with valid XML
+  my $parser = XML::LibXML->new();
+  my $doc = eval { $parser->parse_balanced_chunk($input) };
+  if ($@) {    # Didn't parse, so invalid XML string
+    throw RDF::Trine::Error -text => "$@";
+  }
 
-        # Last chance is that it is a string with valid XML
-        my $parser = XML::LibXML->new();
-        my $doc = eval { $parser->parse_balanced_chunk( $input ) };
-        if ($@) { # Didn't parse, so invalid XML string
-	  throw RDF::Trine::Error -text => "$@";
-	}
+  if ($lang) {
+    foreach my $context ( $doc->childNodes ) {
+      $context->setAttributeNS( XML_XML_NS, 'lang', $lang );
+    }
+    $input = $doc->toString;
+  }
 
-	if ($lang) {
-	  foreach my $context ($doc->childNodes) {
-	    $context->setAttributeNS(XML_XML_NS, 'lang', $lang);
-	  }
-	  $input = $doc->toString;
-	}
+  my $self = $class->SUPER::_new( $input, undef,
+    'http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral' );
 
-        my $self = $class->SUPER::_new( $input, undef, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral' );
-
-        $XML_FRAGMENTS{ refaddr( $self ) }	= $doc;
-        return $self;
+  $XML_FRAGMENTS{ refaddr($self) } = $doc;
+  return $self;
 }
-
-
 
 =item C<< xml_element >>
 
@@ -149,35 +155,34 @@ Returns the XML::LibXML node for the XML Literal.
 =cut
 
 sub xml_element {
-	my $self	= shift;
-	my $node	= $XML_FRAGMENTS{ refaddr( $self ) };
-	unless (blessed($node)) {
-		throw RDF::Trine::Error -text => "No XML element found for object";
-	}
-	return $node;
+  my $self = shift;
+  my $node = $XML_FRAGMENTS{ refaddr($self) };
+  unless ( blessed($node) ) {
+    throw RDF::Trine::Error -text => "No XML element found for object";
+  }
+  return $node;
 }
 
 # Check if we have an acceptable type
 sub _check_type {
   my $type = shift;
   return 0 unless blessed($type);
-  return ($type->isa('XML::LibXML::Document') ||
-	  $type->isa('XML::LibXML::DocumentFragment') ||
-	  $type->isa('XML::LibXML::Element') ||
-	  $type->isa('XML::LibXML::CDATASection') ||
-	  $type->isa('XML::LibXML::Text') ||
-	  $type->isa('XML::LibXML::NodeList') );
+  return ( $type->isa('XML::LibXML::Document')
+      || $type->isa('XML::LibXML::DocumentFragment')
+      || $type->isa('XML::LibXML::Element')
+      || $type->isa('XML::LibXML::CDATASection')
+      || $type->isa('XML::LibXML::Text')
+      || $type->isa('XML::LibXML::NodeList') );
 }
 
-
 sub DESTROY {
-	my $self	= shift;
-	delete $XML_FRAGMENTS{ refaddr( $self ) };
-	
-	if ($self->can('SUPER::DESTROY')) {
-		$self->SUPER::DESTROY();
-	}
-	return;
+  my $self = shift;
+  delete $XML_FRAGMENTS{ refaddr($self) };
+
+  if ( $self->can('SUPER::DESTROY') ) {
+    $self->SUPER::DESTROY();
+  }
+  return;
 }
 
 1;
